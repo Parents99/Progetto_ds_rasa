4922631634da6961080cd429b7257b9c32216778
Parents99 <christianparente6@gmail.com>
2024-04-19 18:09:26 +0200

    ilike nelle query

--- actions/actions.py
+++ actions/actions.py
@@ -13,6 +13,7 @@ import os
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 from dotenv import load_dotenv
 
@@ -25,7 +26,7 @@ DB_NAME = os.environ.get("dbname")
 USER = os.environ.get("user")
 PASSWORD = os.environ.get("password")
 
-#conn2 = pg.connect(database = DB_NAME, user = USER, password = PASSWORD)
+#connection = pg.connect(database = DB_NAME, user = USER, password = PASSWORD)
 
 connection = pg.connect("dbname=progetto_rasa user=postgres password=prova")
 cursor = connection.cursor()
@@ -54,18 +55,23 @@ class ActionRecommendGenre(Action):
         return "recommend_genre"
     
     def run(self, dispatcher : CollectingDispatcher, tracker : Tracker, domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
-        genre = tracker.get_slot("genre")
-        query = f"SELECT name FROM serie_tv WHERE genres_1 = '{genre}'  OR genres_2 = '{genre}' ORDER BY vote_count DESC LIMIT 20"
+        genre : str = tracker.get_slot("genre")
+
+        if not genre:
+            pass #messagio di genere non capito
+            return []
+
+        query = f"SELECT name FROM serie_tv WHERE genres_1 ILIKE '{genre}'  OR genres_2 ILIKE '{genre}' ORDER BY vote_count DESC LIMIT 20"
         cursor.execute(query)
         data = cursor.fetchall()
 
         if data:
-            names : list = [row[2] for row in data]
+            names : list = [row[2] for row in data] #### da cambiare
             dispatcher.utter_message("here some {} tv series:\n- {}".format(genre, "\n- ".join(names))) #forse basta
         else:
             dispatcher.utter_message("I'm sorry, I couldn't find TV series with the specified genre.")
 
-        return []
+        return [SlotSet("genre", genre)] # forse non serve
         #for rows in data:
 
 
@@ -89,6 +95,7 @@ class ActionRecommendTopRating(Action):
         return []
     
 
+
 #per informazioni su una serie tv
 class ActionInfoSeries(Action):
     def name(self) -> Text:
@@ -99,23 +106,28 @@ class ActionInfoSeries(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         # Estrai il valore dell'entità 'serie_tv' dal tracker
-        serie_tv = next(tracker.get_latest_entity_values("serie_tv"), None)
+        serie_tv : str = next(tracker.get_latest_entity_values("serie_tv"), None)
 
         if not serie_tv:
             dispatcher.utter_message("Non ho capito su quale serie TV stai chiedendo informazioni.")
             return []
 
-        query = f"SELECT DINSTICT name,overview,in_production,original_language,number_of_seasons,number_of_episodes,vote_count FROM serie_tv WHERE name = '{serie_tv}'"
+        query = f"SELECT DINSTICT name,overview,in_production,original_language,number_of_seasons,number_of_episodes,vote_count FROM serie_tv WHERE name ILIKE '{serie_tv}'"
         cursor.execute(query)
         data = cursor.fetchall()
+        
+        rating = data[2] #il 2 a caso, cambiare
+        genre = data[5] # a caso
+
 
         if data:
             dispatcher.utter_message(f"Hai chiesto informazioni su {serie_tv}.")
             ##### messaggio con dati query
+            dispatcher.utter_message(f"data.. {data}") ##### da cambiare
         else:
             dispatcher.utter_message("Non ho trovato la serie TV che stai chiedendo informazioni.")
 
-        return []
+        return [SlotSet("rating", rating)]
 
 
 
@@ -128,8 +140,8 @@ class ActionKidsSeries(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        query = "SELECT name FROM serie_tv WHERE genres_1 = 'Animation'  OR genres_2 = 'Animation' OR genres_1 = 'kids' OR  \
-                genres_2 = 'kids' ORDER BY vote_count DESC LIMIT 20"
+        query = "SELECT name FROM serie_tv WHERE genres_1 = 'Animation'  OR genres_2 = 'Animation' OR genres_1 = 'Kids' OR  \
+                genres_2 = 'Kids' ORDER BY vote_count DESC LIMIT 20"
         
         cursor.execute(query)
         data = cursor.fetchall()
